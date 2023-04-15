975196a0db131b3a2d8a2a8a5004d878b806d04c
atallah-salah <atallah_salah@outlook.com>
2023-04-15 08:43:40 +0300

    fix table issue

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,69 +5,71 @@ const Table = () => {
     <div className={styles.table}>
       <div className={styles.container}>
         <table>
-          <tr>
-            <th>Description</th>
-            <th>Owner</th>
-            <th>Impacted KPI</th>
-            <th>Impacted Sub-KPI</th>
-            <th>Impacted VC step</th>
-            <th>Geography</th>
-          </tr>
-          <tr>
-            <td className={styles.raw}>
-              <img alt="check" src="assets/icons/check.svg" />
-              Review process with Makkah Amanah to improve their Regulation enforcement rate
-            </td>
-            <td>Enforcement Team</td>
-            <td>Regulation enforcement rate​</td>
-            <td>Regulation clause​​</td>
-            <td>Plan / Detect</td>
-            <td>Riyadh</td>
-          </tr>
-          <tr>
-            <td className={styles.raw}>
-              <img alt="check" src="assets/icons/alert.svg" />
-              Review process with Makkah Amanah to improve their Regulation enforcement rate
-            </td>
-            <td>Enforcement Team</td>
-            <td>Regulation enforcement rate​</td>
-            <td>Regulation clause​​</td>
-            <td>Plan / Detect</td>
-            <td>Riyadh</td>
-          </tr>
-          <tr>
-            <td className={styles.raw}>
-              <img alt="check" src="assets/icons/error.svg" />
-              Review process with Makkah Amanah to improve their Regulation enforcement rate
-            </td>
-            <td>Enforcement Team</td>
-            <td>Regulation enforcement rate​</td>
-            <td>Regulation clause​​</td>
-            <td>Plan / Detect</td>
-            <td>Riyadh</td>
-          </tr>
-          <tr>
-            <td className={styles.raw}>
-              <img alt="check" src="assets/icons/check.svg" />
-              Review process with Makkah Amanah to improve their Regulation enforcement rate
-            </td>
-            <td>Enforcement Team</td>
-            <td>Regulation enforcement rate​</td>
-            <td>Regulation clause​​</td>
-            <td>Plan / Detect</td>
-            <td>Riyadh</td>
-          </tr>
-          <tr>
-            <td className={styles.raw}>
-              <img alt="check" src="assets/icons/alert.svg" />
-              Review process with Makkah Amanah to improve their Regulation enforcement rate
-            </td>
-            <td>Enforcement Team</td>
-            <td>Regulation enforcement rate​</td>
-            <td>Regulation clause​​</td>
-            <td>Plan / Detect</td>
-            <td>Riyadh</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Description</th>
+              <th>Owner</th>
+              <th>Impacted KPI</th>
+              <th>Impacted Sub-KPI</th>
+              <th>Impacted VC step</th>
+              <th>Geography</th>
+            </tr>
+            <tr>
+              <td className={styles.raw}>
+                <img alt="check" src="assets/icons/check.svg" />
+                Review process with Makkah Amanah to improve their Regulation enforcement rate
+              </td>
+              <td>Enforcement Team</td>
+              <td>Regulation enforcement rate​</td>
+              <td>Regulation clause​​</td>
+              <td>Plan / Detect</td>
+              <td>Riyadh</td>
+            </tr>
+            <tr>
+              <td className={styles.raw}>
+                <img alt="check" src="assets/icons/alert.svg" />
+                Review process with Makkah Amanah to improve their Regulation enforcement rate
+              </td>
+              <td>Enforcement Team</td>
+              <td>Regulation enforcement rate​</td>
+              <td>Regulation clause​​</td>
+              <td>Plan / Detect</td>
+              <td>Riyadh</td>
+            </tr>
+            <tr>
+              <td className={styles.raw}>
+                <img alt="check" src="assets/icons/error.svg" />
+                Review process with Makkah Amanah to improve their Regulation enforcement rate
+              </td>
+              <td>Enforcement Team</td>
+              <td>Regulation enforcement rate​</td>
+              <td>Regulation clause​​</td>
+              <td>Plan / Detect</td>
+              <td>Riyadh</td>
+            </tr>
+            <tr>
+              <td className={styles.raw}>
+                <img alt="check" src="assets/icons/check.svg" />
+                Review process with Makkah Amanah to improve their Regulation enforcement rate
+              </td>
+              <td>Enforcement Team</td>
+              <td>Regulation enforcement rate​</td>
+              <td>Regulation clause​​</td>
+              <td>Plan / Detect</td>
+              <td>Riyadh</td>
+            </tr>
+            <tr>
+              <td className={styles.raw}>
+                <img alt="check" src="assets/icons/alert.svg" />
+                Review process with Makkah Amanah to improve their Regulation enforcement rate
+              </td>
+              <td>Enforcement Team</td>
+              <td>Regulation enforcement rate​</td>
+              <td>Regulation clause​​</td>
+              <td>Plan / Detect</td>
+              <td>Riyadh</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
